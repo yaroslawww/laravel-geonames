c972b4f5fc05d33c807b2b5e74872988a3c52e9a
Yaro WebDev <yg@think.studio>
2021-09-24 09:58:43 +0300

    Add comments

--- src/Facades/GeoDataImporter.php
+++ src/Facades/GeoDataImporter.php
@@ -4,6 +4,11 @@ namespace LaraGeoData\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static bool storageCreateFromUrl(string $url, ?string $name, ?\Closure $progressCallback = null, bool $force = false)
+ * @method static bool storageExtractZipFile(string $file)
+ * @method static bool storageTruncate()
+ */
 class GeoDataImporter extends Facade
 {
     protected static function getFacadeAccessor()
